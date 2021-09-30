4c4e730da31409af7ca7671a6c3c0e5e895d0a9b
ChemFer <annazuzanna7@gmail.com>
2021-09-30 21:10:57 +0200

    add special characters to password

--- src/com/emailApp/Password.java
+++ src/com/emailApp/Password.java
@@ -2,7 +2,7 @@ package com.emailApp;
 
 public class Password {
     private String password;
-    private final int passwordLength = 10;
+    private final int passwordLength = 12;
 
     public Password() {
         this.password = passwordGenerator(passwordLength);
@@ -26,19 +26,25 @@ public class Password {
 
     private static char randomCharacter(){
         int ascii = 0;
-        int rand = (int) (Math.random() * 62); //10 digits, 26 uppercase letters, 26 lowercase letters
+        int rand = (int) (Math.random() * 72); // 10 special characters, 10 digits, 26 uppercase letters, 26 lowercase letters
         if (rand <= 9){
-            ascii = rand + 48; //number between 0-9, ASCII: 48-57
+            ascii = rand + 33; //special character, ASCII: 33-42
             return (char) ascii;
         }
-        else if (rand > 9 && rand <= 35){
-            //uppercase letters: 10-35, ASCII: 65-90, 65-10=55
-            ascii = rand + 55;
+        else if (rand > 9 && rand <= 19){
+            //number between 10-19, ASCII: 48-57, 48-10=38
+
+            ascii = rand + 38;
+            return (char) ascii;
+        }
+        else if (rand > 19 && rand <= 44){
+            //uppercase letters: 20-45, ASCII: 65-90, 65-20=45
+            ascii = rand + 45;
             return (char) ascii;
         }
         else {
-            //lowercase letters 36-61, ASCII: 97-122, 97-36=61
-            ascii = rand + 61;
+            //lowercase letters 46-71, ASCII: 97-122, 97-46=51
+            ascii = rand + 51;
             return (char) ascii;
         }
     }
